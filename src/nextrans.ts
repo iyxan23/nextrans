6f1e63abfe6074504e27408d24588e9ad01ed972
iyxan23 <nurihsanalghifari@gmail.com>
2024-08-06 14:28:27 +0700

    chore: add base url to nextrans classs

--- src/nextrans.ts
+++ src/nextrans.ts
@@ -2,7 +2,10 @@ import { ConfigurationError } from "./error";
 
 type EnvironmentType = "sandbox" | "production";
 
-interface NextransAccessKeys {
+const SANDBOX_BASE_URL = "https://app.sandbox.midtrans.com";
+const PRODUCTION_BASE_URL = "https://app.midtrans.com";
+
+export interface NextransAccessKeys {
   merchantId: string;
   serverKey: string;
 }
@@ -14,11 +17,14 @@ interface NextransOpts {
 
   environment?: EnvironmentType;
   fetch?: typeof fetch;
+
+  baseUrl?: string;
 }
 
 export class Nextrans {
   private accessKeys: NextransAccessKeys;
   private environment: EnvironmentType;
+  private baseUrl: string;
 
   private fetch: typeof fetch;
 
@@ -28,21 +34,26 @@ export class Nextrans {
     if (typeof fetch === "undefined" && !opts.fetch)
       throw new ConfigurationError(
         "window fetch is undefined, please provide one on the fetch field: `Nextrans({ ..., fetch: customFetch })`",
-      )
+      );
 
     this.fetch = opts.fetch ?? fetch;
 
     switch (this.environment) {
       case "sandbox":
         this.accessKeys = opts.sandbox;
+        this.baseUrl = SANDBOX_BASE_URL;
         break;
+
       case "production":
         if (!opts.production)
           throw new ConfigurationError(
             "environment is production, yet production access keys are undefined.",
           );
         this.accessKeys = opts.production;
+        this.baseUrl = PRODUCTION_BASE_URL;
         break;
     }
+
+    if (opts.baseUrl) this.baseUrl = opts.baseUrl;
   }
 }
