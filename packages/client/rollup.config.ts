4b6738fa72455b7863cb36fc7c070caf625c3d02
iyxan23 <nurihsanalghifari@gmail.com>
2024-08-08 00:10:49 +0700

    fix: building now works on the client package

--- packages/client/rollup.config.ts
+++ packages/client/rollup.config.ts
@@ -4,7 +4,6 @@ import { buildConfig } from '../../scripts/getRollupConfig';
 
 export const input = [
   'src/index.ts',
-  'src/transaction.ts',
 ];
 
 export default function rollup(): RollupOptions[] {
---
+++ packages/client/src/index.ts
@@ -0,0 +1,1 @@
+export * as NextransClient from "./client";
